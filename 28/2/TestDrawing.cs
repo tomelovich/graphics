3b4d1a5b8da966519ea0fe53f5d60af27bcbb3d7
tomelovich <tomelovichs@gmail.com>
2022-06-21 20:46:56 +0300

    Выполнено

---
+++ 28/2/TestDrawing.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _2
+{
+    class TestDrawing : Control
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 1), new Point(-300, -100), 10, 10);
+            drawingContext.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 1), new Point(-300, -100), 20, 20);
+            drawingContext.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 1), new Point(-300, -100), 30, 30);
+            Rect rec = new Rect(-300, -50, Width = 30, Height = 50);
+            Rect rec1 = new Rect(-280, -25, Width = 30, Height = 50);
+            Rect rec2 = new Rect(-260, 0, Width = 30, Height = 50);
+            Rect rec3 = new Rect(-240, 25, Width = 30, Height = 50);
+            Rect rec4 = new Rect(-220, 50, Width = 30, Height = 50);
+
+            Rect rec5 = new Rect(-200, -150, Width = 30, Height = 30);
+            Rect rec6 = new Rect(-170, -150, Width = 30, Height = 30);
+            Rect rec7 = new Rect(-140, -150, Width = 30, Height = 30);
+            Rect rec8 = new Rect(-200, -120, Width = 30, Height = 30);
+            Rect rec9 = new Rect(-170, -120, Width = 30, Height = 30);
+            Rect rec10 = new Rect(-140, -120, Width = 30, Height = 30);
+            Rect rec11 = new Rect(-200, -90, Width = 30, Height = 30);
+            Rect rec12 = new Rect(-170, -90, Width = 30, Height = 30);
+            Rect rec13 = new Rect(-140, -90, Width = 30, Height = 30);
+            Rect rec14 = new Rect(40, 90, Width = 110, Height = 50);
+
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1),rec);
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), rec1);
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), rec2);
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), rec3);
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), rec4);
+
+            drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1), rec5);
+            drawingContext.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), rec6);
+            drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1), rec7);
+            drawingContext.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), rec8);
+            drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1), rec9);
+            drawingContext.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), rec10);
+            drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1), rec11);
+            drawingContext.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), rec12);
+            drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1), rec13);
+
+            drawingContext.DrawRoundedRectangle(Brushes.Black, new Pen(Brushes.Black, 1), rec14,12,12);
+            base.OnRender(drawingContext);
+        }
+    }
+}
---
+++ 28/28/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < 50; i++)
+            {
+                g.DrawEllipse(new Pen(Brushes.Black), 20, 20, 200, 200);
+                g.DrawEllipse(new Pen(Brushes.Black), 45, 45, 150, 150);
+                g.DrawEllipse(new Pen(Brushes.Black), 67, 67, 100, 100);
+
+                g.DrawRectangle(new Pen(Brushes.Black), 300, 60, 50, 100);
+                g.DrawRectangle(new Pen(Brushes.Black), 320, 120, 50, 100);
+                g.DrawRectangle(new Pen(Brushes.Black), 340, 180, 50, 100);
+                g.DrawRectangle(new Pen(Brushes.Black), 360, 240, 50, 100);
+                g.DrawRectangle(new Pen(Brushes.Black), 380, 300, 50, 100);
+
+                g.DrawRectangle(new Pen(Brushes.Black), 499, 99, 151, 151);
+                g.FillRectangle(new SolidBrush(Color.Black),500, 100, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.White), 550, 100, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.Black), 600, 100, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.White), 500, 150, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.Black), 550, 150, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.White), 600, 150, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.Black), 500, 200, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.White), 550, 200, 50, 50);
+                g.FillRectangle(new SolidBrush(Color.Black), 600, 200, 50, 50);
+
+                g.FillEllipse(new SolidBrush(Color.Black), 50, 230, 50, 50);
+            }
+        }
+    }
+}
